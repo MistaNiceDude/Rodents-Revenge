8141d6f727fd7a4eadf9564b1f43bd75064ff510
Desmondchaser27 <browndesmond27@gmail.com>
2022-01-24 23:53:09 -0500

    Fixing the issue where the corners of the screen would keep colored objects/rects drawn on the screen.  Just needed to call WIN.fill(0) before everything else to refresh the screen before drawing.

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class Rat:
         self.hits = hits
         self.rat_img = RAT
         self.mask = pygame.mask.from_surface(self.rat_img)
-        self.rect = self.rat_img.get_rect()
+        self.rect = pygame.Rect(self.rat_img.get_rect())
         self.vel = 7
         self.angle = 0 
 
@@ -151,10 +151,16 @@ def main():
     player_pos = (player.x, player. y)
 
     def redraw_window():
+        #Since the window is the thing drawing everything, fill it blank before every redraw to avoid
+        #   that pesky issue where colored rects stay drawn in black/background corners.
+        WIN.fill(0)
         WIN.blit(BG, (0,0))
+        player.rect = player.rect.move(1, 0)
+        print(player.rect)
         player.draw(WIN)
        # wood.draw(WIN)
         wood.draw(WIN)
+        #pygame.draw.rect(WIN, pygame.color.Color(200, 0, 0), player.rect)
         hits_label = hits_font.render(f"Hits: {hits}", 1, (149,11,9))
         level_label = level_font.render(f"Level: {level}", 1, (149,11,9))
         clock_label = clock_font.render(f"FPS: {clock}", 1, (149,11, 9))
@@ -162,6 +168,7 @@ def main():
         WIN.blit(hits_label, (20,20))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 20, 20))
         WIN.blit(clock_label, ( 20,HEIGHT - clock_label.get_height() - 20,))
+        #WIN.blit(player.rect)
         #screen.blit(RAT, (WIDTH/2, HEIGHT/2))
         pygame.display.update()
 
